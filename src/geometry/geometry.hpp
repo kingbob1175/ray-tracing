bd88d650604829f1b565f8ed88e793cfd9637102
kingbob1175 <3571148400@qq.com>
2025-08-08 13:13:08 +0800

    Delete src directory

--- src/geometry/geometry.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "bound3.hpp"
-#include "normal.hpp"
-#include "vector.hpp"
-#include "point.hpp"
--- src/renderer.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "scene.hpp"
-#include "camera/camera.hpp"
-
-class Renderer {
-public:
-    void Render(const Scene& scene, Camera camera);
-};
